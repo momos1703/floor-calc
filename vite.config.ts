9bbca56157d06f1b5a15333592f4b115322dc55a
Ruslan Hrynchuk <r.momos@gmail.com>
2025-02-11 15:52:57 +0200

    Update project configuration and repository name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vite_ts_calc_volume_self-leveling_floor/',
+  base: '/floor-calc/',
   plugins: [react()],
 })
