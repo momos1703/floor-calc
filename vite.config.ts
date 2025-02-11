091fcf8920d962e7548c6da79fe578d27d21a715
Ruslan Hrynchuk <r.momos@gmail.com>
2025-02-11 15:38:13 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/vite_ts_calc_volume_self-leveling_floor/',
+  plugins: [react()],
+})
